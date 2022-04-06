056fd55fff86f15e1f3a22a4d364953b50999809
betolarbac <betocr.dev@gmail.com>
2022-04-05 22:48:42 -0300

    seção sobre

---
+++ src/components/Sobre/index.jsx
@@ -0,0 +1,62 @@
+import styles from './sobre.module.scss';
+import { FiDownloadCloud, FiTwitter} from 'react-icons/fi';
+import { HiOutlineMail} from 'react-icons/hi';
+import { FaInstagram, FaWhatsapp} from 'react-icons/fa';
+
+
+export function Sobre() {
+  return (
+    <main className={styles.contentContainer}>
+
+    <img src="/images/perfil.png" alt="looper"/>
+
+
+    <section className={styles.hero}>
+      <h1>Sobre mim</h1>
+      <h2>Alagoas, Brasil</h2>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+      Praesent id egestas dui. Nullam finibus aliquam enim quis <br />
+      faucibus. Aenean ac commodo dolor, nec bibendum velit.
+      </p>
+      <br />
+      <a href="">
+      <FiTwitter color="#00D2DF" />
+      </a>
+      <a href="">
+      <FaInstagram color="#00D2DF" />
+      </a>
+      <a href="">
+      <FaWhatsapp color="#00D2DF" />
+      </a>
+      <br />
+
+      
+
+      <button 
+      type="button" 
+      className={styles.button1}>
+          
+     <a href="https://www.linkedin.com/in/roberto-larbac/">
+     <FiDownloadCloud color="white" />
+     Currículo
+     </a>
+    </button>
+
+    <button 
+      type="button"
+      >
+      <a href="https://github.com/betolarbac">
+      <HiOutlineMail color="white" />
+      E-mail
+      </a>
+    </button>
+
+    </section>
+
+    
+
+  
+  </main>
+  )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,39 +1,42 @@
 import styles from './home.module.scss';
 import { FiLinkedin, FiGithub} from 'react-icons/fi';
-
+import {Sobre} from '../components/Sobre'
 
 export default function Home() {
   return (
+    <>
     <main className={styles.contentContainer}>
-    <section className={styles.hero}>
-      <p>Olá, eu sou</p>
-      <h1>Roberto Cabral</h1>
-      <p>
-        Desenvolvedor Front-end. <br/>
-      </p>
+      <section className={styles.hero}>
+        <p>Olá, eu sou</p>
+        <h1>Roberto Cabral</h1>
+        <p>
+          Desenvolvedor Front-end.<br />
+        </p>
 
-      <button 
-      type="button">
-     <a href="https://www.linkedin.com/in/roberto-larbac/">
-     <FiLinkedin color="white" />
-      LinkedIn
-     </a>
-    </button>
+        <button
+          type="button">
+          <a href="https://www.linkedin.com/in/roberto-larbac/">
+            <FiLinkedin color="white" />
+            LinkedIn
+          </a>
+        </button>
 
-    <button 
-      type="button"
-      className={styles.button1}>
-      <a href="https://github.com/betolarbac">
-      <FiGithub color="white" />
-      GitHub
-      </a>
-    </button>
+        <button
+          type="button"
+          className={styles.button1}>
+          <a href="https://github.com/betolarbac">
+            <FiGithub color="white" />
+            GitHub
+          </a>
+        </button>
 
-    </section>
+      </section>
 
-    <img src="/images/Looper.svg" alt="looper"/>
+      <img src="/images/Looper.svg" alt="looper" />
 
-  
-  </main>
+
+    </main>
+    <Sobre />
+    </>
   )
 }
