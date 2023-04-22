3edb2e859715c1cbe7fc07f74140fe6f20c5cc12
roberto cabral <betocr.dev@gmail.com>
2023-04-22 17:34:16 -0300

    ajustes de configuração

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,43 +1,46 @@
-import styles from './styles.module.scss';
-import Link from 'next/link';
-import { motion } from "framer-motion"
+import styles from "./styles.module.scss";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 export function Header() {
-
-  const menuPath =  window.location.pathname;
-
-    return(
-      <header className={styles.headerContainer}>
-        <div className={styles.headerContent}>
-        <Link href="/">
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/" passHref>
           <motion.h1
-            initial={{ y: -250}}
+            initial={{ y: -250 }}
             animate={{ y: 10 }}
-            transition={{delay: 0.2}}
-          >Betocbl</motion.h1>
-          </Link >
+            transition={{ delay: 0.2 }}
+          >
+            Betocbl
+          </motion.h1>
+        </Link>
+        <nav>
+          <Link href="/">
+            <a >Home</a>
+          </Link>
 
-          <nav>
+          <Link href="/sobre">
+            <a >
+              Sobre
+            </a>
+          </Link>
 
-            <Link href="/">
-              <a className={`${menuPath === "/" ? styles.active : ""}`}>Home</a>
-              </Link >
+          <Link href="/experiencia">
+            <a
+             
+            >
+              Experiência
+            </a>
+          </Link>
 
-              <Link href="/sobre">
-              <a className={`${menuPath === "/sobre" ? styles.active : ""}`}>Sobre</a>
-              </Link>
-
-              <Link href="/experiencia">
-              <a className={`${menuPath === "/experiencia" ? styles.active : ""}`}>Experiência</a>
-              </Link>
-
-              <Link href="/projeto">
-               <a className={`${menuPath === "/projeto" ? styles.active : ""}`}>Projetos</a>
-              </Link>
-              
-          </nav>
-          
-        </div>
-      </header>
-    );
-  }
+          <Link href="/projeto">
+            <a >
+              Projetos
+            </a>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
